152dd0b7d024cc0a4d84da22d9efbae38ce0c8f5
Johanna149 <j.schindler@htlkrems.at>
2026-04-24 12:17:08 +0200

    version 24.4.26

--- Code/Rauchmelder/main/oneshot_read_main.c
+++ Code/Rauchmelder/main/oneshot_read_main.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "soc/soc_caps.h"
@@ -14,41 +15,27 @@
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
 
-const static char *TAG = "EXAMPLE";
+const static char *TAG = "MQ2";
 
 /*---------------------------------------------------------------
         ADC General Macros
 ---------------------------------------------------------------*/
-//ADC1 Channels
 #if CONFIG_IDF_TARGET_ESP32
 #define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_6
-//#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_5
-//#else
-//#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_2
-//#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_3
 #endif
-
-//#if (SOC_ADC_PERIPH_NUM >= 2) && !CONFIG_IDF_TARGET_ESP32C3
-/**
- * On ESP32C3, ADC2 is no longer supported, due to its HW limitation.
- * Search for errata on espressif website for more details.
- */
-//#define EXAMPLE_USE_ADC2            1
-//#endif
-
-//#if EXAMPLE_USE_ADC2
-//ADC2 Channels
-//#if CONFIG_IDF_TARGET_ESP32
-//#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
-//#else
-//#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
-//#endif
-//#endif  //#if EXAMPLE_USE_ADC2
 
 #define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_12
 
-static int adc_raw[10]; //speicher für rohwerte
-static int voltage[10]; //speicher für spannungswerte
+/*---------------------------------------------------------------
+        MQ-2 Einstellungen
+---------------------------------------------------------------*/
+#define MQ2_VCC     5.0f     // Versorgungsspannung vom MQ-2 Modul
+#define MQ2_RL      5.0f     // Lastwiderstand in kOhm, oft 5k oder 10k
+#define MQ2_R0      10.0f    // Platzhalter, später kalibrieren!
+
+static int adc_raw[10];
+static int voltage[10];
+
 static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
 static void example_adc_calibration_deinit(adc_cali_handle_t handle);
 
@@ -67,80 +54,56 @@ void app_main(void)
         .bitwidth = ADC_BITWIDTH_DEFAULT,
     };
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));
-    //ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN1, &config));
-    
+
     //-------------ADC1 Calibration Init---------------//
     adc_cali_handle_t adc1_cali_chan0_handle = NULL;
-    //adc_cali_handle_t adc1_cali_chan1_handle = NULL;
-    bool do_calibration1_chan0 = example_adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC1_CHAN0, EXAMPLE_ADC_ATTEN, &adc1_cali_chan0_handle);
-    //bool do_calibration1_chan1 = example_adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC1_CHAN1, EXAMPLE_ADC_ATTEN, &adc1_cali_chan1_handle);
+    bool do_calibration1_chan0 = example_adc_calibration_init(
+        ADC_UNIT_1,
+        EXAMPLE_ADC1_CHAN0,
+        EXAMPLE_ADC_ATTEN,
+        &adc1_cali_chan0_handle
+    );
 
-  /*#if EXAMPLE_USE_ADC2
-    //-------------ADC2 Init---------------//
-    adc_oneshot_unit_handle_t adc2_handle;
-    adc_oneshot_unit_init_cfg_t init_config2 = {
-        .unit_id = ADC_UNIT_2,
-        .ulp_mode = ADC_ULP_MODE_DISABLE,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config2, &adc2_handle));
-
-    //-------------ADC2 Calibration Init---------------//
-    adc_cali_handle_t adc2_cali_handle = NULL;
-    bool do_calibration2 = example_adc_calibration_init(ADC_UNIT_2, EXAMPLE_ADC2_CHAN0, EXAMPLE_ADC_ATTEN, &adc2_cali_handle);
-
-    //-------------ADC2 Config---------------//
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, EXAMPLE_ADC2_CHAN0, &config));*/
-//#endif  //#if EXAMPLE_USE_ADC2
-
-   while (1) {
+    while (1) {
         ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0]));
-        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0]);
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d",
+                 ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0]);
 
-        //Umrechnung in Spannung
         if (do_calibration1_chan0) {
-        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan0_handle, adc_raw[0], &voltage[0]));
-        ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, voltage[0]);
+            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(
+                adc1_cali_chan0_handle,
+                adc_raw[0],
+                &voltage[0]
+            ));
+
+            ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV",
+                     ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, voltage[0]);
+
+            //-------------MQ-2 Berechnung---------------//
+            float vout = voltage[0] / 1000.0f;   // mV -> V
+
+            if (vout > 0.01f) {
+                float rs = MQ2_RL * (MQ2_VCC - vout) / vout;
+                float ratio = rs / MQ2_R0;
+
+                ESP_LOGI(TAG, "MQ2 Vout: %.3f V", vout);
+                ESP_LOGI(TAG, "MQ2 Rs: %.3f kOhm", rs);
+                ESP_LOGI(TAG, "MQ2 Rs/R0: %.3f", ratio);
+
+                // Später für Datenbank z. B.:
+                // adc_raw[0]
+                // voltage[0]
+                // vout
+                // rs
+                // ratio
+            } else {
+                ESP_LOGW(TAG, "MQ2 Vout zu klein, Berechnung übersprungen");
+            }
         }
+
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
-
-       /* ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN1, &adc_raw[1]));
-        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN1, adc_raw[1]);
-        if (do_calibration1_chan1) {
-            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan1_handle, adc_raw[1], &voltage[1]));
-            ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN1, voltage[1]);
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));*/
-       
-
-/*#if EXAMPLE_USE_ADC2
-        ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, EXAMPLE_ADC2_CHAN0, &adc_raw[1][0]));
-        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_2 + 1, EXAMPLE_ADC2_CHAN0, adc_raw[1][0]);
-        if (do_calibration2) {
-            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc2_cali_handle, adc_raw[1][0], &voltage[1][0]));
-            ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_2 + 1, EXAMPLE_ADC2_CHAN0, voltage[1][0]);
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));*/
-/*#endif  //#if EXAMPLE_USE_ADC2
-    }
-
-    //Tear Down
-    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
-    if (do_calibration1_chan0) {
-        example_adc_calibration_deinit(adc1_cali_chan0_handle);
-    }
-    if (do_calibration1_chan1) {
-        example_adc_calibration_deinit(adc1_cali_chan1_handle);
-    }
-
-#if EXAMPLE_USE_ADC2
-    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc2_handle));
-    if (do_calibration2) {
-        example_adc_calibration_deinit(adc2_cali_handle);
-    }
-#endif //#if EXAMPLE_USE_ADC2
-}*/
 
 /*---------------------------------------------------------------
         ADC Calibration
@@ -183,9 +146,9 @@ static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel,
 #endif
 
     *out_handle = handle;
+
     if (ret == ESP_OK) {
         ESP_LOGI(TAG, "Calibration Success");
-
     } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
         ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
     } else {
